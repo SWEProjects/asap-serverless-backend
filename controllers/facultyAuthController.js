7d038f16534c4428693d34ad67db20f6497c1361
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-09-10 22:07:44 +0530

    chore  : added jwt tokens

--- controllers/facultyAuthController.js
+++ controllers/facultyAuthController.js
@@ -1,15 +1,21 @@
 const db = require('../config/db');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
-
+const JWT_SECRET = process.env.JWT_SECRET
 const loginFaculty = async (req, res) => {
     try {
         const { collegeId , password } = req.body;
+        if (!collegeId || !password) {
+            return res.status(400).json({ message: 'Please provide collegeId and password' });
+        }
         const result = await db.query('SELECT * FROM faculties WHERE f_college_id = $1', [collegeId]);
         if (result.rows.length && await bcrypt.compare(password, result.rows[0].f_password)) {
+            const token = jwt.sign({ facultyId: result.rows[0].fid }, JWT_SECRET, { expiresIn: '1h' });
             return res.status(200).json({
                 success: true,
-                message: 'Faculty Login Success'
+                message: 'Faculty Login Success',
+                token: token
             });
         }
         return res.status(400).json({ message: 'Invalid credentials' });
@@ -21,19 +27,26 @@ const loginFaculty = async (req, res) => {
 const registerFaculty = async (req, res) => {
     try {
         const { firstName, lastName, collegeId, password } = req.body;
+        if (!firstName ||!collegeId ||!password) {
+            return res.status(400).json({ message: 'Please provide all the required fields' });
+        }
         const checkFaculty = await db.query('SELECT * FROM faculties WHERE f_college_id = $1',[collegeId]);
         if (checkFaculty.rows.length) {
             return res.status(400).json({ message: 'Faculty already exists' });
         }
         const hashedPassword = await bcrypt.hash(password, 10);
         try{
-            await db.query('INSERT INTO faculties (f_first_name, f_last_name, f_college_id, f_password) VALUES ($1,$2,$3,$4)',[firstName, lastName, collegeId ,hashedPassword]);
+            await db.query('BEGIN')
+            const faculty = await db.query('INSERT INTO faculties (f_first_name, f_last_name, f_college_id, f_password) VALUES ($1,$2,$3,$4) RETURNING fid',[firstName, lastName || null, collegeId ,hashedPassword]);
+            const token = jwt.sign({ facultyId: faculty.rows[0].fid }, JWT_SECRET, { expiresIn: '1h' });
+            await db.query('COMMIT')
             return res.status(200).json({
                 success : true,
-                message: 'Faculty Registration Success'
+                message: 'Faculty Registration Success',
+                token:token
             });
-        } catch (e) {
-            return res.status(400).json({ message: 'Faculty registration failed', error :e });
+        } finally {
+
         }
 
         
--- controllers/studentAuthController.js
+++ controllers/studentAuthController.js
@@ -1,15 +1,22 @@
 const db = require('../config/db');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
+const JWT_SECRET = process.env.JWT_SECRET
 
 const loginStudent = async (req, res) => {
     try {
         const { collegeId , password, secret } = req.body;
+        if (!collegeId || !password || !secret) {
+            return res.status(400).json({ message: 'Missing required fields' });
+        }
         const result = await db.query('SELECT * FROM students WHERE s_college_id = $1 AND s_device_id = $2', [collegeId, secret]);
         if (result.rows.length && await bcrypt.compare(password, result.rows[0].s_password)) {
+            const token = jwt.sign({ studentId: result.rows[0].sid }, JWT_SECRET, { expiresIn: '1h' });
             return res.status(200).json({
                 success: true,
-                message: 'Student Login Success'
+                message: 'Student Login Success',
+                token: token
             });
         }
         return res.status(400).json({ message: 'Invalid credentials' });
@@ -21,18 +28,26 @@ const loginStudent = async (req, res) => {
 const registerStudent = async (req, res) => {
     try {
         const { firstName, lastName, rollNumber, semester, collegeId, password, secret } = req.body;
+        if (!firstName || !rollNumber || !semester || !collegeId || !password || !secret) {
+            return res.status(400).json({ message: 'Missing required fields' });
+        }
         const checkStudent = await db.query('SELECT * FROM students WHERE s_college_id = $1',[collegeId]);
         if (checkStudent.rows.length) {
             return res.status(400).json({ message: 'Student already exists' });
         }
         const hashedPassword = await bcrypt.hash(password, 10);
         try{
-            await db.query('INSERT INTO students (s_first_name, s_last_name, s_college_id, s_password, s_device_id, s_roll_number, s_semester) VALUES ($1,$2,$3,$4,$5,$6,$7)',[firstName, lastName, collegeId ,hashedPassword, secret, rollNumber, semester]);
+            await db.query('BEGIN')
+            const student = await db.query('INSERT INTO students (s_first_name, s_last_name, s_college_id, s_password, s_device_id, s_roll_number, s_semester) VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING sid',[firstName, lastName || null, collegeId ,hashedPassword, secret, rollNumber, semester]);
+            const token = jwt.sign({ studentId: student.rows[0].sid }, JWT_SECRET, { expiresIn: '1h' });
+            await db.query('COMMIT')
             return res.status(200).json({
                 success : true,
-                message: 'Student Registration Success'
+                message: 'Student Registration Success',
+                token:token
             });
         } catch (e) {
+            await db.query('ROLLBACK')
             return res.status(400).json({ message: 'Student registration failed', error :e });
         }
 
