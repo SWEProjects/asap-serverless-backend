6e014929a3f4330f9220e25ad927e8b3bf602144
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-10-26 11:24:25 +0530

    chore : passed the studentId to front end while registering student

--- controllers/studentAuthController.js
+++ controllers/studentAuthController.js
@@ -44,7 +44,8 @@ const registerStudent = async (req, res) => {
             return res.status(200).json({
                 success : true,
                 message: 'Student Registration Success',
-                token:token
+                token:token,
+                studentId: student.rows[0].sid
             });
         } catch (e) {
             await db.query('ROLLBACK')
