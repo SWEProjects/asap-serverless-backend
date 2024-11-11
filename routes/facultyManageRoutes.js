54dbe612dae4e30f07161d59e7400e60443594ed
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-11-11 12:58:53 +0530

    feat : added route to get current courses of faculty

--- routes/facultyManageRoutes.js
+++ routes/facultyManageRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { editCourses , editProfile} = require('../controllers/facultyManageController');
+const { editCourses , editProfile, getCurrentCourses} = require('../controllers/facultyManageController');
 
 const router = express.Router();
 
 router.post('/course/edit', editCourses);
 router.post('/profile/edit', editProfile);
-
+router.get('/current/courses',getCurrentCourses);
 
 module.exports = router;
