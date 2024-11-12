9ddd7fb6c8639e2617bd258d45b01dffdfe10669
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-11-12 13:47:21 +0530

    feat : added route to get departments

--- controllers/commonManageController.js
+++ controllers/commonManageController.js
@@ -40,4 +40,23 @@ const getAllCourses = async (req,res) => {
     }
 }
 
-module.exports = {getBatches, getAllCourses}
+const getDepartments = async (req, res) => {
+    try {
+        const token = req.headers.authorization
+        if (!token){
+            return res.status(403).json({ message: 'No JWT Provided' })
+        }
+        const decoded = jwt.verify(token, JWT_SECRET)
+        try {
+            const departments = await db.query('SELECT d_name AS department_name, did AS department_id FROM departments');
+            res.status(200).json({status : 200, departments : departments.rows});
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid JWT' });
+    }
+}
+
+
+module.exports = {getBatches, getAllCourses, getDepartments}
